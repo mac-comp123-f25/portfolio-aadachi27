7ea48c7d9952456437ceaeb295cd373533fd48a7
aadachi27 <aadachi@macalester.edu>
2025-09-05 10:28:51 -0500

    bugs ica done

--- src/ica/01-python+ide/buggy_scripts/script_c.py
+++ src/ica/01-python+ide/buggy_scripts/script_c.py
@@ -1,6 +1,7 @@
 # This program converts celsius to fahrenheit
+# changed fraction from 5/9 to 9/5 to correctly calculate fahrenheit
 
 cels_temp = 30
-fraction = 5/9
+fraction = 9/5
 fahr_temp = (cels_temp * fraction) + 32
 print(fahr_temp)
