c7250e63fd97e27b43163a0171491bfd6d58e5c3
aadachi27 <aadachi@macalester.edu>
2025-09-08 10:07:50 -0500

    banana.py

---
+++ src/ica/02-data_types/banana.py
@@ -0,0 +1,8 @@
+# banana chain length calculator
+
+banana_length = 15.5
+num_bananas = 5100
+
+total_length_m = (banana_length * num_bananas) / 100
+
+print("The total length would be", total_length_m , "meters.")
