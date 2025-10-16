f75d28453defdf7356b1be22c1a3df8ea86bfd16
aadachi27 <aadachi@macalester.edu>
2025-10-16 18:42:09 -0500

    08-iteration done

--- src/ica/08-iteration/whileloops.py
+++ src/ica/08-iteration/whileloops.py
@@ -17,12 +17,29 @@ def print_every_other(x):
     # when indentation stops, while loop is over
     print("Done!")
 
+def print_every_fifth(x):
+    '''
+    prints every fifth value from x down to
+    '''
+    while x >= 0:
+        print(x)
+        x = x - 5
+    print("Done!")
+
+print_every_fifth(20)
 
 def square_user_nums():
     """
     Reads in numbers from the user, stopping when the user enters a negative
     number. For each user number, it prints the number and the square of
     the number.
+
+    If the user_inp and user_num definitions are taken out from the beginning,
+    the while loop isn't able to run because user_num hasn't been defined.
+
+    If the user_inp and user_num definitions are taken out of the while loop,
+    the user is only asked to input a number once, and the print statement keeps
+    running forever.
     """
     user_inp = input("Enter the next number (negative to quit): ")
     user_num = int(user_inp)
@@ -31,6 +48,15 @@ def square_user_nums():
         user_inp = input("Enter the next number (negative to quit): ")
         user_num = int(user_inp)
 
+def square_user_nums2():
+    while True:
+        user_inp = input("Enter the next number (negative to quit): ")
+        user_num = int(user_inp)
+        if user_num < 0:
+            break
+        print(user_num, "squared is", user_num ** 2)
+
+print(square_user_nums2())
 
 def sum_to_n(top_num):
     """
@@ -44,6 +70,16 @@ def sum_to_n(top_num):
         curr_val = curr_val + 3
 
     return total
+
+def add_user_nums():
+    sum_of_nums = 0
+    user_num = int(input("enter a number, enter 0 to end "))
+    while user_num != 0:
+        sum_of_nums = sum_of_nums + user_num
+        user_num = int(input("enter a number, enter 0 to end "))
+    return(sum_of_nums)
+
+print(add_user_nums())
 
 
 def next_word(text):
